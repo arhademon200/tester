b34fa73ef1521c28cc8f7de2f09d1fbe6182b421
arhademon200 <85450617+arhademon200@users.noreply.github.com>
2022-12-14 16:29:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
-
-file=$(1.conf)
+#touch 1.conf
+#echo "3" >> 1.conf
+#echo "10 7" >> 1.conf
+#echo "5 11" >> 1.conf
+#echo "6 8" >> 1.conf
+file="1.conf"
+pLength=()
 while read line; do
-    for value in $line
-    do
-        echo -e "$line\n"
+    parameters=()
+    amount=0
+    lines=()
+    echo -e "$line\n"
+    for value in $line; do
+        parameters+=($value)
+        let amount++
     done
-done < $file
-
-
-
-
-#chmod 777 main.c
+    pLength+=(${parameters[0]})
+    lines+=(${parameters[1]})
+   
+    
+    #echo -e "$parameters\n"
+    #echo -e "$lines\n"
+    #echo -e "$amount\n"
+    #echo -e "$pLength\n"
+    
+done <$file 
+touch wynik.txt
+echo  "`./permutacje/main.c $parameters` "  >> ./permutacje/wynik.txt 
