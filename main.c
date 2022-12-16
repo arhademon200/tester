72240f21d98b70f8fdd8f7565ea0ccc1d626e43f
arhademon200 <85450617+arhademon200@users.noreply.github.com>
2022-12-16 21:41:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,57 +6,15 @@
 #include<ctype.h>
 #include<string.h>
 
-int main(int argc, const char *const *argv[]){
-	int x=argv[1]
-	int y = 1;
-	int zbior[x];
+int main (int argc,const char *const argv[])
+{
+/*
+printf("%s",argv[1]);
+*/
 
-	for (int i = 0; i < x; i++)
-	{	
-		zbior[i] = y;
-		y++;
-	}
-	int permutacja[x];
-	int cnt1 = x-1;
+int dlugosc = atoi(argv[1]);
+int ilosc = atoi(argv[2]);
+printf("%d",dlugosc);
+printf("%d",ilosc);
 
-	for (int i = 0; i < x; i++)
-	{
-
-		srand(time(NULL));	
-    	int l = rand() % (cnt1 + 1);
-		permutacja[i] = zbior[l];
-		
-		int j = l;
-		while(j < cnt1 + 1)
-		{
-			zbior[j] = zbior[j+1];
-			j++;
-		}
-		zbior[cnt1] = cnt1 + 1;
-		cnt1--;
-	}
-	
-
-	for (int i = 0; i < x; i++) 
-	{
-		int dlg_z = floor(log10(zbior[i])) + 1;
-		int dlg_p = floor(log10(permutacja[i])) + 1;
-		if (dlg_z < dlg_p)
-		{
-			int cnt = dlg_p - dlg_z;
-			while (cnt < dlg_p)
-			{
-				printf(" ");
-				cnt++;
-			}
-		
-		}
-	}
-	printf("\n");
-	
-		for (int i = 0; i < x; i++) 
-	{
-		printf("%d ", permutacja[i]);
-	}
-	printf("\n");
 }
