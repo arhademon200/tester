3f545cfa12e326339d6d3197e885b595e60989bb
LucjuszRosso <120335687+LucjuszRosso@users.noreply.github.com>
2022-12-17 00:29:18 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,7 +14,52 @@ printf("%s",argv[1]);
 
 int dlugosc = atoi(argv[1]);
 int ilosc = atoi(argv[2]);
-printf("%d",dlugosc);
-printf("%d",ilosc);
+//printf("%d",dlugosc);
+//printf("%d",ilosc);
 
+	int x = dlugosc;
+	int y = 1;
+	
+	int zbior[x];
+
+	for (int i = 0; i < x; i++)
+	{	
+		zbior[i] = y;
+		y++;
+	}
+
+
+	int permutacja[x];
+	int cnt1 = x-1;
+
+	for (int i = 0; i < x; i++)
+	{
+
+		srand(time(NULL));	
+    	int l = rand() % (cnt1 + 1);
+
+		permutacja[i] = zbior[l];
+		
+		int j = l;
+		while(j < cnt1 + 1)
+		{
+			zbior[j] = zbior[j+1];
+			j++;
+		}
+		zbior[cnt1] = cnt1 + 1;
+		
+		cnt1--;
+	}
+	
+
+	
+		for (int i = 0; i < x; i++) 
+	{
+		printf("%d ", permutacja[i]);
+	}
+
+	return 0;
 }
+
+
+
