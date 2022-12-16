b5161aa3ea236ea87b35a7b54297ba7814c6ba8f
arhademon200 <85450617+arhademon200@users.noreply.github.com>
2022-12-16 21:39:42 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
+#mkdir make
+#cd make
+chmod 777 CMakeLists.txt
+cmake .
+make 
+chmod 777 ./main
 #touch 1.conf
 #echo "3" >> 1.conf
 #echo "10 7" >> 1.conf
 #echo "5 11" >> 1.conf
 #echo "6 8" >> 1.conf
 file="1.conf"
-pLength=()
 while read line; do
     parameters=()
+    dlug=()
     amount=0
-    lines=()
-    echo -e "$line\n"
+    ilo=()
+    #echo -e "$line\n"
     for value in $line; do
         parameters+=($value)
         let amount++
     done
-    pLength+=(${parameters[0]})
-    lines+=(${parameters[1]})
-   
-    
-    #echo -e "$parameters\n"
-    #echo -e "$lines\n"
-    #echo -e "$amount\n"
-    #echo -e "$pLength\n"
-    
-done <$file
-
-
-chmod 777 CMakeLists.txt
-cmake .
-make 
-chmod 777 ./main.c
-
-
-echo -e "`./main.c ${parameters[1]}`"
+    dlug+=(${parameters[0]})
+    ilo+=(${parameters[1]})
+    echo "`./main ${parameters[0]} ${parameters[1]} }`"
+done <$file 
