0bdbf024f7574d07c5652a2e315e1b17e9c7ed35
arhademon200 <85450617+arhademon200@users.noreply.github.com>
2022-12-18 20:44:10 +0100

    Update cmake.sh

--- cmake.sh
+++ cmake.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+needed_files=("cycle.c" "main.c" "next.c" "order.c" "parity.c" "previous.c" "square.c" "twoline.c") 
+for need in ${needed_files[@]}
+do
+	if ! [ -e $need ]
+	then
+		echo "Nie mozna znalesc wymaganego pliku $need"
+		exit 4
+	fi
+done
 chmod 777 CMakeLists.txt
 chmod 777 ./start.sh
 cmake .
