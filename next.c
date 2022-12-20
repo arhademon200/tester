61819937a0e44f41766fc63fbb1c7819a15eb492
LucjuszRosso <120335687+LucjuszRosso@users.noreply.github.com>
2022-12-20 01:20:48 +0100

    Update next.c

--- next.c
+++ next.c
@@ -4,32 +4,26 @@
 #include<math.h>
 
 
-int main()
+int main (int argc,const char *const argv[])
 {
-	int x, y = 0;
-	float z;
-	printf("Podaj dlugosc zbioru: ");
-	scanf("%d", &x);
+/*
+printf("%s",argv[1]);
+*/
 
-	int zbior[x];
+	int dlugosc = atoi(argv[1]);
+	int x = dlugosc;
 
-    while(y < x)
-	{
-		printf("Podaj %d element zbioru (liczba naturalna): ", y);
-		if(scanf("%f", &z) == 1 && z >= 0 && fmod(z,1) == 0)
-		{
-			zbior[y] = z;
-			y++;
-		}
-		else
-			printf("To nie jest liczba naturalna.\n");
-	}
+	int permutacja[dlugosc];
+	int p = 0;
 	
-	for (int i = 0; i < x; i++) 
+	for (int o = 2; o < dlugosc + 2; o++)
 	{
-		printf("%d ", zbior[i]);
+		permutacja[p] = atoi(argv[o]);
+		p++;
 	}
-	printf("\n");
+
+
+	/* NASTEPNA */
 
 
         int n = x-1;
@@ -42,10 +36,10 @@ int main()
 
         for (j = 0; 0 <= j && j <= n - 1; j++)
         {
-            if(zbior[j] < zbior[j+1])
+            if(permutacja[j] < permutacja[j+1])
             {	if(j >= j_aj)
                 {
-                    aj = zbior[j];
+                    aj = permutacja[j];
                     j_aj = j;
                     jest_j = true;
                 }	
@@ -59,26 +53,26 @@ int main()
         {
             for (int k = 0; k < x; k++)
             {
-                if(zbior[k] > aj && k > j_aj)
-                    if(zbior[k] <= ak)
+                if(permutacja[k] > aj && k > j_aj)
+                    if(permutacja[k] <= ak)
                     {
-                        ak = zbior[k];
+                        ak = permutacja[k];
                         k_ak = k;
                     }
             }
 
-            temp = zbior[j_aj];
-            zbior[j_aj] = zbior[k_ak];
-            zbior[k_ak] = temp;
+            temp = permutacja[j_aj];
+            permutacja[j_aj] = permutacja[k_ak];
+            permutacja[k_ak] = temp;
 
             int m = x - j_aj;
             int z = j_aj+1;
             int w = x - 1;
             for(int i = 0; i < m/2 ; i++)
             {
-                temp = zbior[z];
-                zbior[z] = zbior[w];
-                zbior[w] = temp;
+                temp = permutacja[z];
+                permutacja[z] = permutacja[w];
+                permutacja[w] = temp;
                 z++;
                 w--;
             }
@@ -91,13 +85,11 @@ int main()
         {
             for (int i = 0; i < x; i++) 
             {
-                printf("%d ", zbior[i]);
+                printf("%d ", permutacja[i]);
             }
         }
         else
             printf("Nie ma.");
 
-        printf("\n");
-}
 
-	
+}
