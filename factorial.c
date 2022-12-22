3a7e2cca4217b126ec213c5ef043bc264316d152
arhademon200 <85450617+arhademon200@users.noreply.github.com>
2022-12-22 12:51:54 +0100

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -6,8 +6,6 @@ int main(int argc,const char *const argv[])
     int n = atoi(argv[1]);
 
     int silnia = 1;
-    printf("Podaj ilosc: ");
-    scanf("%d", &n);
 
     for (int i = 1; (i <= n) && (silnia <= 50); ++i) 
     {
