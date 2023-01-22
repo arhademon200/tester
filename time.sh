37997c571540a04b108748ecfa5af9b87d06c1ca
arhademon200 <85450617+arhademon200@users.noreply.github.com>
2023-01-22 11:38:52 +0100

    Delete time.sh

--- time.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p czastmp
-{ time ./start.sh 1 ; } 2> czastmp/tmp1.txt
-
-grep 'real' czastmp/tmp1.txt | grep -v 'user\|sys' > wynik.txt
-sed -i -e 's/[real \t]*//' wynik.txt
-sed -i 's/m/\n/;s/s//' wynik.txt
-rm -r czastmp
